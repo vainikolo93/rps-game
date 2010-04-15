aae6f917e9342df6adcb8ed91d18587508095043
kahweh@gmail.com <kahweh@gmail.com@b8896358-3fff-e460-0c7d-b4de98c2518b>
2010-04-15 14:42:52 +0000

    add ui assets

--- trunk/android/src/com/kahweh/rps/RockPaperScissors.java
+++ trunk/android/src/com/kahweh/rps/RockPaperScissors.java
@@ -2,6 +2,8 @@ package com.kahweh.rps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 public class RockPaperScissors extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,8 @@ public class RockPaperScissors extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView tv = (TextView)findViewById(R.id.text1);
+        DisplayMetrics dm = this.getApplicationContext().getResources().getDisplayMetrics();
+        tv.setText(String.valueOf(dm.widthPixels) + "X" + String.valueOf(dm.heightPixels));
     }
 }
